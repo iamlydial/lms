c4f3d667d9c8f3ef4f668281e49c6819a175b168
iamlydial <lydia.lavecchia@gmail.com>
2024-11-06 21:12:06 +0100

    feat: add isAuthenticated middleware
    - add function responsible for verifying that a user is authenticated by checking a JWT token stored in the user's cookies
    - extended the Express.Request  interface to make TypeScript aware of this custom user property

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -17,7 +17,7 @@ export const sendToken = (user: IUser, statusCode: number, res: Response) => {
   const refreshToken = user.SignRefreshToken();
 
   //upload session to redis
-  redis.set(user._id, JSON.stringify(user) as any);
+  redis.set(user.id, JSON.stringify(user) as any);
 
   // parse environment variables to integrate with fallback values
   const accessTokenExpire = parseInt(
