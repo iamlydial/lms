898f49e6a860ed485fbd56482e139110d7fa3a98
iamlydial <lydia.lavecchia@gmail.com>
2024-11-12 09:43:43 +0100

    feat: add update access token

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -7,17 +7,20 @@ import {
   registrationUser,
   socialAuth,
   updateAccessToken,
+  updatePassword,
   updateUserInfo,
 } from "../controllers/user.controller";
-import {  isAuthenticated } from "../middleware/auth";
+import { isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
 
 userRouter.post("/registration", registrationUser);
 userRouter.post("/activate-user", activateUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/logout", isAuthenticated, logoutUser);
-userRouter.get("/me", isAuthenticated, getUserInfo)
-userRouter.post("/social-auth", socialAuth)
-userRouter.put("/update-user-info", isAuthenticated, updateUserInfo)
+userRouter.get("/refresh-token", isAuthenticated, updateAccessToken);
+userRouter.get("/me", isAuthenticated, getUserInfo);
+userRouter.post("/social-auth", socialAuth);
+userRouter.put("/update-user-info", isAuthenticated, updateUserInfo);
+userRouter.put("/update-user-password", isAuthenticated, updatePassword);
 
 export default userRouter;
