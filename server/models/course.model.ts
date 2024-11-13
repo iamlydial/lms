a7c252eda2ff802e79e4331037f3b5ab8905d7cc
iamlydial <lydia.lavecchia@gmail.com>
2024-11-13 10:31:10 +0100

    feat(models): add Course interface for Mongoose schema and integrate related interfaces
    - Define ICourse interface to represent course details such as name, description, price, tags, and level
    - Add support for course structure with benefits, prerequisites, and reviews
    - Integrate IReview, ICourseData, and ILink interfaces for structured review and content management
    - Added optional properties for ratings and purchased metrics

--- server/models/course.model.ts
+++ server/models/course.model.ts
@@ -12,20 +12,41 @@ interface IReview extends Document {
   commentReplies: IComment[];
 }
 
-interface ILink extends Document{
-    title: string; 
-    url: string;
+interface ILink extends Document {
+  title: string;
+  url: string;
 }
 
 interface ICouseData extends Document {
+  title: string;
+  description: string;
+  videoUrl: string;
+  videoThumbnail: object;
+  videoSection: string;
+  videoLength: number;
+  videoPlayer: string;
+  links: ILink[];
+  suggestion: string;
+  questions: IComment[];
+}
+
+interface ICourse extends Document {
+  name: string;
+  description: string;
+  price: number;
+  estimatedprice: number;
+  thumbnail: object;
+  tags: string;
+  level: string;
+  demoUrl: string;
+  benefits: {
     title: string;
-    description: string;
-    videoUrl: string; 
-    videoThumbnail: object;
-    videoSection: string;
-    videoLength: number;
-    videoPlayer: string;
-    links: ILink[];
-    suggestion: string;
-    questions: IComment[]; 
+  }[];
+  prerequisites: {
+    title: string;
+  }[];
+  reviews: IReview[];
+  courseData: ICouseData[];
+  ratings?: number;
+  purchased?: number;
 }
