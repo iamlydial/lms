639f9e580e3dbac34f20f0a188e81f8deed05add
iamlydial <lydia.lavecchia@gmail.com>
2024-11-13 10:19:24 +0100

    feat(models): add Review and Comment interfaces for Mongoose schema
    - Define IReview interface to represent user reviews with rating, comment, and replies
    - Define IComment interface to represent user comments and replies on reviews

---
+++ server/models/course.model.ts
@@ -0,0 +1,13 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+interface IComment extends Document {
+  user: object;
+  comment: string;
+}
+
+interface IReview extends Document {
+  user: object;
+  rating: number;
+  comment: string;
+  commentReplies: IComment[];
+}
