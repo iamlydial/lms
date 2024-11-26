c83858e862d3002823b6bae6735e06e3a8d6412e
iamlydial <lydia.lavecchia@gmail.com>
2024-11-26 14:15:53 +0100

    feat: add get course by id route and controller when course is not purchased and use not authenticated

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,9 @@
 import express from "express";
-import { editCourse, uploadCourse } from "../controllers/course.controller";
+import {
+  editCourse,
+  getSingleCourse,
+  uploadCourse,
+} from "../controllers/course.controller";
 import { authorizedRoles, isAuthenticated } from "../middleware/auth";
 const courseRouter = express.Router();
 
@@ -17,4 +21,5 @@ courseRouter.put(
   editCourse
 );
 
+courseRouter.get("/get-course/:id", getSingleCourse);
 export default courseRouter;
