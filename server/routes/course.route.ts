814f275113b285a9a8c5cbae4a0e0315146ff5a8
iamlydial <lydia.lavecchia@gmail.com>
2024-11-26 12:32:58 +0100

    feat: add courseRouter for handling course-related routes
    - uses middleware to ensure that only authenticated users with specific roles (ex: "admin") can perform this action.

---
+++ server/routes/course.route.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { uploadCourse } from "../controllers/course.controller";
+import { authorizedRoles, isAuthenticated } from "../middleware/auth";
+const courseRouter = express.Router();
+
+courseRouter.post(
+  "/create-course",
+  isAuthenticated,
+  authorizedRoles("admin"),
+  uploadCourse
+);
+
+export default courseRouter;
