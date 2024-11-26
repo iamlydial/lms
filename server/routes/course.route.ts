05f7c317e9f9319aef71bc550bc0829a71956afb
iamlydial <lydia.lavecchia@gmail.com>
2024-11-26 13:55:02 +0100

    feat: add update course controller
    - add put route to update the course

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { uploadCourse } from "../controllers/course.controller";
+import { editCourse, uploadCourse } from "../controllers/course.controller";
 import { authorizedRoles, isAuthenticated } from "../middleware/auth";
 const courseRouter = express.Router();
 
@@ -8,6 +8,13 @@ courseRouter.post(
   isAuthenticated,
   authorizedRoles("admin"),
   uploadCourse
+);
+
+courseRouter.put(
+  "/update-course/:id",
+  isAuthenticated,
+  authorizedRoles("admin"),
+  editCourse
 );
 
 export default courseRouter;
