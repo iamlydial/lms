47ee9e987d4236ae3f678b80f04b35210d904fde
iamlydial <lydia.lavecchia@gmail.com>
2024-11-26 18:03:36 +0100

    feat: add route and controller for the valid user

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # lms
  Learning Management System Full stack. LMS platform with typescript, node js, express js, MongoDB, Redis, next 13,rtk query, socket io.
-//5 14 51 / 8 54 59
--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -125,3 +125,34 @@ export const getAllCourses = CatchAsyncError(
     }
   }
 );
+
+//get course content -- only for valid users
+export const getCourseByUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userCourseList = req.user?.courses;
+      const courseId = req.params.id;
+
+      console.log("Course id: ", courseId)
+
+      const courseExist = userCourseList?.find(
+        (course: any) => course._id.toString() === courseId
+      );
+
+      if (!courseExist) {
+        return next(
+          new ErrorHandler("You are not eligible to access this course", 404)
+        );
+      }
+
+      const course = await CourseModel.findById(courseId);
+
+      res.status(200).json({
+        success: true,
+        course,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
